58d000b0caa014ce6d1db24244f9966b912b17e3
varsanydavid20 <varsanyid.24d@acsjszki.hu>
2026-01-14 08:44:01 +0100

    Update valami.py

--- 01.14/valami.py
+++ 01.14/valami.py
@@ -1,7 +1,15 @@
+"""1
 szo = input("Adj meg egy szót: ")
 maganhangzok = "aáeéiíoóöőuúüű"
 db = 0
 for karakter in szo:
     if karakter.lower() in maganhangzok:
         db += 1
-print(f"A szóban {db} magánhangzó van.")
+print(f"A szóban {db} magánhangzó van.")    """
+
+"""2
+szöveg = input("Szöveg: ")
+
+print(szöveg .split())
+print(len(szöveg .split()))
+    """
