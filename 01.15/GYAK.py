dabb950352bfd2bdaae6c1dd4ff5fc43534bcedd
varsanydavid20 <varsanyid.24d@acsjszki.hu>
2026-01-15 12:26:45 +0100

    Create GYAK.py

---
+++ 01.15/GYAK.py
@@ -0,0 +1,7 @@
+"""szamok = ["3,24","1,33","4,5","3,33","4,25"]
+szamokcsere = [float(szam.replace(",",".")) for szam in szamok]
+atlag = sum(szamokcsere) / len(szamokcsere)
+print(f"A számok átlaga: {atlag}")
+"""
+
+print("Ird be a szöveget:")
