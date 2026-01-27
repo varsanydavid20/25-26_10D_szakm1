7f3366670c125723985e257b4cd105c50ace5adc
varsanydavid20 <varsanyid.24d@acsjszki.hu>
2026-01-27 14:26:35 +0100

    Create gak.py

---
+++ 01.27/gak.py
@@ -0,0 +1,21 @@
+"""def koszon(a):
+    return "Helló "+a+"!"
+    
+nev = input("Név:")
+print(koszon(nev))"""
+
+def kivon():
+    None
+
+def osszead():
+    None
+
+def szoroz():
+    None
+
+def oszt():
+    None
+
+szam1 = int(input("Kérem az elsőszámot: "))
+szam2 = int(input("Kérem a második számot: "))
+muvjel=input("Kérem a művelet jelét")
