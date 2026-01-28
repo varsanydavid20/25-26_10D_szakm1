74f84f1b97880ac885d86b84947b0d766cf2fbf2
varsanydavid20 <varsanyid.24d@acsjszki.hu>
2026-01-28 08:26:18 +0100

    Create feladat.py

---
+++ 01.28/feladat.py
@@ -0,0 +1,16 @@
+import random
+
+lista = [random.randint(0,9) for _ in range(10)]
+print("Eredeti lista:", lista)
+
+paratlanok_szama = sum(1 for x in lista if x % 2 == 1)
+print("Páratlan számok száma:", paratlanok_szama)
+
+unique_lista = list(set(lista))
+print("Ismétlődők eltávolítva:", unique_lista)
+
+minden_szam = set(range(10))  
+hianyak = sorted(minden_szam - set(lista))
+
+print("Hiányzó számok 0-9 között:", hianyak if hianyak else "Nincs hiányzó szám")
+
